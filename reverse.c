8056e4f5f4e565d486e8bbb032ee046490cee4a5
shivani soni <shivanisoni12284@gmail.com>
2025-03-22 22:09:00 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,39 @@
+// #include<stdio.h>
+// int main()
+// {
+//     char c[20]="XML";
+//     char d[20];
+//     d[0]=c[2];
+//     d[1]=c[1];
+//     d[2]=c[0];
+//     printf("the reverse of %c%c%c is %c%c%c", c[0],c[1],c[2],d[0],d[1],d[2]);
+//     return 0;
+
+
+// }
+
+//one more way
+#include<stdio.h>
+int main()
+{
+    char c1 = 'X';
+    char c2 = 'M';
+    char c3 = 'L';
+    printf("the reverse of %c%c%c is %c%c%c",c1,c2,c3,c3,c2,c1);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
