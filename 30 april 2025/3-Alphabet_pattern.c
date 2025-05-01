744ef00540c34d6b5c0cd132aa280e9d587f49b1
shivani soni <shivanisoni12284@gmail.com>
2025-05-01 08:22:08 +0530

    Update 3-Alphabet_pattern.c

--- 30 april 2025/3-Alphabet_pattern.c
+++ 30 april 2025/3-Alphabet_pattern.c
@@ -8,29 +8,6 @@
 int main()
 {
     int i,j,k,temp=65;
-    // for(i=1;i<=4;i++)
-    // {
-    //     // int f=t;
-    //     n=g;
-    //     k=g;
-    //     for(;k<=n;k++)
-    //     {
-    //         int f=t;
-    //         j=s;
-    //         for(;j<65+k+f;j++)
-    //         {
-    //             printf("%c ",j);
-    //         }
-    //         s=j;
-    //         // printf("%d",s);
-    //         // printf("\n");
-    //         t=f+t+1;
-    //         // printf("..%d",t);
-    //     }
-    //     g=k;
-    //     printf("\n");
-
-
     for(i=1;i<=4;i++)
     {
         j=temp;
@@ -48,9 +25,6 @@ int main()
         // printf("%d",temp);
         printf("\n");
      }
-
-        
-    
     return 0;
 }
    
